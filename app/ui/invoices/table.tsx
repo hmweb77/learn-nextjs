095c8d2c095c1627bbdaf8b89998272ca2bb78df
hmweb77 <oussamahamdani1@gmail.com>
2024-07-22 12:36:30 +0100

    first commit

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -91,7 +91,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt="table"
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
